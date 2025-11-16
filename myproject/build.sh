917655590f6cdeff5a862e5df02b5682a4db1f47
6710615201 <6710615201@student.tu.ac.th>
2025-11-16 13:13:37 +0700

    Update build.sh

--- myproject/build.sh
+++ myproject/build.sh
@@ -4,4 +4,5 @@ pip install --upgrade pip
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py makemigrations myapp
+python manage.py makemigrations room
 python manage.py migrate 
